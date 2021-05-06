c4ce0bf52ba6f9273a2a9abec6e641ca2777a02d
param <param.catchchaos@gmail.com>
2021-05-06 22:58:13 +0100

    Use 4x downsampling for HR and 16x for LR

--- train_nerf.py
+++ train_nerf.py
@@ -39,17 +39,54 @@ def main():
     # torch.autograd.set_detect_anomaly(True)
 
     # If a pre-cached dataset is available, skip the dataloader.
+    USE_CACHED_DATASET = False
+    train_paths, validation_paths = None, None
+    images, poses, render_poses, hwf, i_split = None, None, None, None, None
+    H, W, focal, i_train, i_val, i_test = None, None, None, None, None, None
     if hasattr(cfg.dataset, "cachedir") and os.path.exists(cfg.dataset.cachedir):
         train_paths = glob.glob(os.path.join(cfg.dataset.cachedir, "train", "*.data"))
-    if hasattr(cfg.dataset, "hr_fps_factor"):
-        USE_HR_LR = True
-        train_paths = train_paths[::cfg.dataset.hr_fps_factor]
-        train_paths_lr = glob.glob(os.path.join(cfg.dataset.cachedir_lr, "train", "*.data"))
+        validation_paths = glob.glob(
+            os.path.join(cfg.dataset.cachedir, "val", "*.data")
+        )
+        USE_CACHED_DATASET = True
     else:
-        USE_HR_LR = False
-    validation_paths = glob.glob(
-        os.path.join(cfg.dataset.cachedir, "val", "*.data")
-    )
+        # Load dataset
+        images, poses, render_poses, hwf = None, None, None, None
+        if cfg.dataset.type.lower() == "blender":
+            images, poses, render_poses, hwf, i_split = load_blender_data(
+                cfg.dataset.basedir,
+                half_res=cfg.dataset.half_res,
+                testskip=cfg.dataset.testskip,
+            )
+            i_train, i_val, i_test = i_split
+            H, W, focal = hwf
+            H, W = int(H), int(W)
+            hwf = [H, W, focal]
+            if cfg.nerf.train.white_background:
+                images = images[..., :3] * images[..., -1:] + (1.0 - images[..., -1:])
+        elif cfg.dataset.type.lower() == "llff":
+            images, poses, bds, render_poses, i_test = load_llff_data(
+                cfg.dataset.basedir, factor=cfg.dataset.downsample_factor
+            )
+            hwf = poses[0, :3, -1]
+            poses = poses[:, :3, :4]
+            if not isinstance(i_test, list):
+                i_test = [i_test]
+            if cfg.dataset.llffhold > 0:
+                i_test = np.arange(images.shape[0])[:: cfg.dataset.llffhold]
+            i_val = i_test
+            i_train = np.array(
+                [
+                    i
+                    for i in np.arange(images.shape[0])
+                    if (i not in i_test and i not in i_val)
+                ]
+            )
+            H, W, focal = hwf
+            H, W = int(H), int(W)
+            hwf = [H, W, focal]
+            images = torch.from_numpy(images)
+            poses = torch.from_numpy(poses)
 
     # Seed experiment for repeatability
     seed = cfg.experiment.randomseed
@@ -133,42 +170,76 @@ def main():
         if model_fine:
             model_coarse.train()
 
-        if USE_HR_LR and i % cfg.dataset.hr_bias != 0:
-            datafile = np.random.choice(train_paths_lr)
-        else:
+        rgb_coarse, rgb_fine = None, None
+        target_ray_values = None
+        if USE_CACHED_DATASET:
             datafile = np.random.choice(train_paths)
-        cache_dict = torch.load(datafile)
-        ray_bundle = cache_dict["ray_bundle"].to(device)
-        ray_origins, ray_directions = (
-            ray_bundle[0].reshape((-1, 3)),
-            ray_bundle[1].reshape((-1, 3)),
-        )
-        target_ray_values = cache_dict["target"][..., :3].reshape((-1, 3))
-        select_inds = np.random.choice(
-            ray_origins.shape[0],
-            size=(cfg.nerf.train.num_random_rays),
-            replace=False,
-        )
-        ray_origins, ray_directions = (
-            ray_origins[select_inds],
-            ray_directions[select_inds],
-        )
-        target_ray_values = target_ray_values[select_inds].to(device)
-        # ray_bundle = torch.stack([ray_origins, ray_directions], dim=0).to(device)
+            cache_dict = torch.load(datafile)
+            ray_bundle = cache_dict["ray_bundle"].to(device)
+            ray_origins, ray_directions = (
+                ray_bundle[0].reshape((-1, 3)),
+                ray_bundle[1].reshape((-1, 3)),
+            )
+            target_ray_values = cache_dict["target"][..., :3].reshape((-1, 3))
+            select_inds = np.random.choice(
+                ray_origins.shape[0],
+                size=(cfg.nerf.train.num_random_rays),
+                replace=False,
+            )
+            ray_origins, ray_directions = (
+                ray_origins[select_inds],
+                ray_directions[select_inds],
+            )
+            target_ray_values = target_ray_values[select_inds].to(device)
+            # ray_bundle = torch.stack([ray_origins, ray_directions], dim=0).to(device)
 
-        rgb_coarse, _, _, rgb_fine, _, _ = run_one_iter_of_nerf(
-            cache_dict["height"],
-            cache_dict["width"],
-            cache_dict["focal_length"],
-            model_coarse,
-            model_fine,
-            ray_origins,
-            ray_directions,
-            cfg,
-            mode="train",
-            encode_position_fn=encode_position_fn,
-            encode_direction_fn=encode_direction_fn,
-        )
+            rgb_coarse, _, _, rgb_fine, _, _ = run_one_iter_of_nerf(
+                cache_dict["height"],
+                cache_dict["width"],
+                cache_dict["focal_length"],
+                model_coarse,
+                model_fine,
+                ray_origins,
+                ray_directions,
+                cfg,
+                mode="train",
+                encode_position_fn=encode_position_fn,
+                encode_direction_fn=encode_direction_fn,
+            )
+        else:
+            img_idx = np.random.choice(i_train)
+            img_target = images[img_idx].to(device)
+            pose_target = poses[img_idx, :3, :4].to(device)
+            ray_origins, ray_directions = get_ray_bundle(H, W, focal, pose_target)
+            coords = torch.stack(
+                meshgrid_xy(torch.arange(H).to(device), torch.arange(W).to(device)),
+                dim=-1,
+            )
+            coords = coords.reshape((-1, 2))
+            select_inds = np.random.choice(
+                coords.shape[0], size=(cfg.nerf.train.num_random_rays), replace=False
+            )
+            select_inds = coords[select_inds]
+            ray_origins = ray_origins[select_inds[:, 0], select_inds[:, 1], :]
+            ray_directions = ray_directions[select_inds[:, 0], select_inds[:, 1], :]
+            # batch_rays = torch.stack([ray_origins, ray_directions], dim=0)
+            target_s = img_target[select_inds[:, 0], select_inds[:, 1], :]
+
+            then = time.time()
+            rgb_coarse, _, _, rgb_fine, _, _ = run_one_iter_of_nerf(
+                H,
+                W,
+                focal,
+                model_coarse,
+                model_fine,
+                ray_origins,
+                ray_directions,
+                cfg,
+                mode="train",
+                encode_position_fn=encode_position_fn,
+                encode_direction_fn=encode_direction_fn,
+            )
+            target_ray_values = target_s
 
         coarse_loss = torch.nn.functional.mse_loss(
             rgb_coarse[..., :3], target_ray_values[..., :3]
@@ -225,22 +296,46 @@ def main():
 
             start = time.time()
             with torch.no_grad():
-                datafile = np.random.choice(validation_paths)
-                cache_dict = torch.load(datafile)
-                rgb_coarse, _, _, rgb_fine, _, _ = run_one_iter_of_nerf(
-                    cache_dict["height"],
-                    cache_dict["width"],
-                    cache_dict["focal_length"],
-                    model_coarse,
-                    model_fine,
-                    cache_dict["ray_origins"].to(device),
-                    cache_dict["ray_directions"].to(device),
-                    cfg,
-                    mode="validation",
-                    encode_position_fn=encode_position_fn,
-                    encode_direction_fn=encode_direction_fn,
-                )
-                target_ray_values = cache_dict["target"].to(device)
+                rgb_coarse, rgb_fine = None, None
+                target_ray_values = None
+                if USE_CACHED_DATASET:
+                    datafile = np.random.choice(validation_paths)
+                    cache_dict = torch.load(datafile)
+                    rgb_coarse, _, _, rgb_fine, _, _ = run_one_iter_of_nerf(
+                        cache_dict["height"],
+                        cache_dict["width"],
+                        cache_dict["focal_length"],
+                        model_coarse,
+                        model_fine,
+                        cache_dict["ray_origins"].to(device),
+                        cache_dict["ray_directions"].to(device),
+                        cfg,
+                        mode="validation",
+                        encode_position_fn=encode_position_fn,
+                        encode_direction_fn=encode_direction_fn,
+                    )
+                    target_ray_values = cache_dict["target"].to(device)
+                else:
+                    img_idx = np.random.choice(i_val)
+                    img_target = images[img_idx].to(device)
+                    pose_target = poses[img_idx, :3, :4].to(device)
+                    ray_origins, ray_directions = get_ray_bundle(
+                        H, W, focal, pose_target
+                    )
+                    rgb_coarse, _, _, rgb_fine, _, _ = run_one_iter_of_nerf(
+                        H,
+                        W,
+                        focal,
+                        model_coarse,
+                        model_fine,
+                        ray_origins,
+                        ray_directions,
+                        cfg,
+                        mode="validation",
+                        encode_position_fn=encode_position_fn,
+                        encode_direction_fn=encode_direction_fn,
+                    )
+                    target_ray_values = img_target
                 coarse_loss = img2mse(rgb_coarse[..., :3], target_ray_values[..., :3])
                 loss, fine_loss = 0.0, 0.0
                 if rgb_fine is not None:
